7a6be93532c15560e9a4b53607e682b2abad44b9
Thibault Jamet <tjamet@users.noreply.github.com>
2025-11-27 11:12:44 +0100

    Add a flag to run time in UTC

--- cmd/ai-reporter/main.go
+++ cmd/ai-reporter/main.go
@@ -17,6 +17,7 @@ import (
 var (
 	configPath = flag.String("config", "config.yaml", "Path to configuration file")
 	startTime  = flag.String("start", "", "Start time for metric collection (RFC3339 format). If not provided, it will be set to the start of previous day")
+	utc        = flag.Bool("utc", true, "Use UTC timezone for the time range. If false, the local timezone will be used.")
 	endTime    = flag.String("end", "", "End time for metric collection (RFC3339 format). If not provided, it will be set to the end of the previous day")
 )
 
@@ -38,21 +39,37 @@ func run() error {
 	// Parse time range
 	var start, end time.Time
 	if *startTime != "" {
-		start, err = time.Parse(time.RFC3339, *startTime)
+		if *utc {
+			start, err = time.ParseInLocation(time.RFC3339, *startTime, time.UTC)
+		} else {
+			start, err = time.Parse(time.RFC3339, *startTime)
+		}
 		if err != nil {
 			return fmt.Errorf("invalid start time: %w", err)
 		}
 	} else {
-		start = time.Now().Truncate(24 * time.Hour).Add(-24 * time.Hour)
+		if *utc {
+			start = start.UTC().Truncate(24 * time.Hour).Add(-24 * time.Hour)
+		} else {
+			start = start.Truncate(24 * time.Hour).Add(-24 * time.Hour)
+		}
 	}
 
 	if *endTime != "" {
-		end, err = time.Parse(time.RFC3339, *endTime)
+		if *utc {
+			end, err = time.ParseInLocation(time.RFC3339, *endTime, time.UTC)
+		} else {
+			end, err = time.Parse(time.RFC3339, *endTime)
+		}
 		if err != nil {
 			return fmt.Errorf("invalid end time: %w", err)
 		}
 	} else {
-		end = time.Now().Truncate(24 * time.Hour).Add(-time.Microsecond)
+		if *utc {
+			end = end.UTC().Truncate(24 * time.Hour).Add(-time.Microsecond)
+		} else {
+			end = end.Truncate(24 * time.Hour).Add(-time.Microsecond)
+		}
 	}
 
 	// Run collection and publishing
